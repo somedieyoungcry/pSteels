6cda5c6beb9fb115d60948c932b70a18efcfb5ea
somedieyoungcry <jfuentess004@alumno.uaemex.mx>
2023-01-09 22:16:07 -0600

    day 1

--- pSteel/steelS/views.py
+++ pSteel/steelS/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'steelS/index.html', {})
+
+def about(request):
+    return render(request, 'steelS/know.html', {})
+
+def jobs(request):
+    return render(request, 'steelS/jobs.html', {})
